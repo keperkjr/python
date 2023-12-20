2d6dc66ed11cad4f429e8598312de0de51cccfbd
keperkjr <keperkjr@yahoo.com>
2023-12-19 22:02:24 -0800

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+# =============================================================================
+#    Author: Kenneth Perkins
+#    Date:   Jul 25, 2013
+#    Taken From: http://programmingnotes.org/
+#    File:  hello.py
+#    Description: Simple hello world program
+# =============================================================================
+def main():
+    print("Hello World, created using Python")
+    input("\nPlease press ENTER to continue...")
+
+if __name__ == "__main__":
+    main()
+# http://programmingnotes.org/
